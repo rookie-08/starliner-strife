83c6c91900cdfccda7d06bf94363890c61324e9d
Rookie <lhn4519@rit.edu>
2026-09-24 13:41:09 -0400

    Sketched how the script will work

--- planets.py
+++ planets.py
@@ -7,13 +7,29 @@ over time.
 
 
 """
+Planet Lists: constant_period, constant_axis, theta, x, y, closest, score
+
+Final Lists: A 2D list, each row is the format for the output file
+
+Main: A large for loop that does the following:
+
+    - Move the planets
+
+    - Calculate the distances and thus the closest
+
+    - Make informed decisions (score add)
+
+    - (score decay)
+
+Main 2: Write the file.
+
+Output file: days passed, score0, score1, score 2, ...
+
 """ # How this script works
 
 
 """
 General Development
-
-- Write how this script works
 
 - Make an upper-level outline as to how the functions are laid out
 
